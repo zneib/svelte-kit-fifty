8331fb9d9e43f62a1311591eb5f5a86569a6b7ce
Zach Neibaur <zneibaur@protonmail.com>
2021-12-28 15:19:29 -0700

    Adding the expanding cards component

---
+++ src/routes/expanding-cards/Panel.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  export let text: String = ''
+  export let imgUrl: String = ''
+  let isActive: Boolean = false
+  console.log('Hello Panel')
+</script>
+
+<div class="panel" class:active={isActive} style="background-image: url({imgUrl})" on:click={() => isActive = !isActive}>
+  <h3>{text}</h3>
+</div>
+
+<style>
+  .panel {
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 80vh;
+    border-radius: 50px;
+    color: #fff;
+    cursor: pointer;
+    flex: 0.5;
+    margin: 10px;
+    position: relative;
+    -webkit-transition: all 700ms ease-in;
+  }
+
+  .panel h3 {
+    font-size: 24px;
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+    margin: 0;
+    opacity: 0;
+  }
+
+  .active {
+    flex: 5;
+  }
+
+  .active h3 {
+    opacity: 1;
+    transition: opacity 0.3s ease-in 0.4s;
+  }
+</style>
---
+++ src/routes/expanding-cards/index.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import Panel from './Panel.svelte'
+  const panels = [
+    {
+      text: 'Explore The World',
+      imgUrl: 'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      text: 'Wild Forest',
+      imgUrl: 'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      text: 'Sunny Beach',
+      imgUrl: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80'
+    },
+    {
+      text: 'City on Winter',
+      imgUrl: 'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80'
+    },
+    {
+      text: 'Mountains - Clouds',
+      imgUrl: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+  ]
+</script>
+
+<div class="container">
+  {#each panels as panel}
+    <Panel text={panel.text} imgUrl={panel.imgUrl} />
+  {/each}
+</div>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
+
+  .container {
+    display: flex;
+    width: 100%;
+    margin: 0 auto;
+  }
+
+  @media (max-width: 480px) {
+    .container {
+      width: 100vw;
+    }
+  }
+</style>
