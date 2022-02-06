f0b2ded21b44c145c03b36b1a9ba9ead6d30e4c2
Zach Neibaur <zneibaur@protonmail.com>
2022-02-06 07:49:57 -0700

    Rotating Nav project added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
@@ -18,15 +17,7 @@
 				<img src="svelte-welcome.png" alt="Welcome" />
 			</picture>
 		</div>
-
-		to your new<br />SvelteKit app
 	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
 </section>
 
 <style>
---
+++ src/routes/rotating-nav/index.svelte
@@ -0,0 +1,168 @@
+<script lang="ts">
+  let showNav = false
+</script>
+
+<div class="container" class:show-nav={showNav}>
+  <div class="circle-container">
+    <div class="circle">
+      <button id="close" on:click={() => showNav = false}>
+        <i class="fas fa-times"></i>
+      </button>
+      <button id="open" on:click={() => showNav = !showNav}>
+        <i class="fas fa-bars"></i>
+      </button>
+    </div>
+  </div>
+
+  <div class="content">
+    <h1>Amazing Article</h1>
+    <small>Florin Pop</small>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia in ratione dolores cupiditate, maxime aliquid impedit dolorem nam dolor omnis atque fuga labore modi veritatis porro laborum minus, illo, maiores recusandae cumque ipsa quos. Tenetur, consequuntur mollitia labore pariatur sunt quia harum aut. Eum maxime dolorem provident natus veritatis molestiae cumque quod voluptates ab non, tempore cupiditate? Voluptatem, molestias culpa. Corrupti, laudantium iure aliquam rerum sint nam quas dolor dignissimos in error placeat quae temporibus minus optio eum soluta cupiditate! Cupiditate saepe voluptates laudantium. Ducimus consequuntur perferendis consequatur nobis exercitationem molestias fugiat commodi omnis. Asperiores quia tenetur nemo ipsa.</p>
+
+    <h3>My Dog</h3>
+    <img src="https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" alt="doggy" />
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit libero deleniti rerum quo, incidunt vel consequatur culpa ullam. Magnam facere earum unde harum. Ea culpa veritatis magnam at aliquid. Perferendis totam placeat molestias illo laudantium? Minus id minima doloribus dolorum fugit deserunt qui vero voluptas, ut quia cum amet temporibus veniam ad ea ab perspiciatis, enim accusamus asperiores explicabo provident. Voluptates sint, neque fuga cum illum, tempore autem maxime similique laborum odio, magnam esse. Aperiam?</p>
+  </div>
+</div>
+
+<nav>
+  <ul>
+    <li><i class="fas fa-home"></i><a href="#"> Home</a></li>
+    <li><i class="fas fa-user-alt"></i><a href="#"> About</a></li>
+    <li><i class="fas fa-envelope"></i><a href="#"> Contact</a></li>
+  </ul>
+</nav>
+
+<style>
+  .container {
+    background-color: #fafafa;
+    transform-origin: top left;
+    transition: transform 0.5s linear;
+    width: 100vw;
+    min-height: 100vh;
+    padding: 50px;
+  }
+
+  .container.show-nav {
+    transform: rotate(-20deg);
+  }
+
+  .circle-container {
+    position: fixed;
+    top: -100px;
+    left: -100px;
+  }
+
+  .circle {
+    background-color: #ff7979;
+    height: 200px;
+    width: 200px;
+    border-radius: 50%;
+    position: relative;
+    transition: transform 0.5s linear;
+  }
+
+  .container.show-nav .circle {
+    transform: rotate(-70deg);
+  }
+
+  .circle button {
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    height: 100px;
+    background: transparent;
+    border: 0;
+    font-size: 26px;
+    color: #fff;
+  }
+
+  .circle button:focus {
+    outline: none;
+  }
+
+  .circle button#open {
+    left: 60%;
+  }
+
+  .circle button#close {
+    top: 60%;
+    transform: rotate(90deg);
+    transform-origin: top left;
+  }
+
+  .container.show-nav + nav li {
+    transform: translateX(0);
+    transition-delay: 0.3s;
+  }
+
+  nav {
+    position: fixed;
+    bottom: 40px;
+    left: 0;
+    z-index: 100;
+  }
+
+  nav ul {
+    list-style-type: none;
+    padding-left: 30px;
+  }
+
+  nav ul li {
+    text-transform: uppercase;
+    color: #fff;
+    margin: 40px 0;
+    transform: translateX(-100%);
+    transition: transform 0.4s ease-in;
+  }
+
+  nav ul li i {
+    font-size: 20px;
+    margin-right: 10px;
+  }
+
+  nav ul li + li {
+    margin-left: 15px;
+    transform: translateX(-150%);
+  }
+
+  nav ul li + li + li {
+    margin-left: 30px;
+    transform: translateX(-200%);
+  }
+
+  nav a{
+    color: #fafafa;
+    text-decoration: none;
+    transition: all 0.5s;
+  }
+
+  nav a:hover {
+    color: #FF7979;
+    font-weight: bold;
+  }
+
+  .content img {
+    max-width: 100%;
+  }
+
+  .content {
+    max-width: 1000px;
+    margin: 50px auto;
+  }
+
+  .content h1 {
+    margin: 0;
+  }
+
+  .content small {
+    color: #555;
+    font-style: italic;
+  }
+
+  .content p {
+    color: #333;
+    line-height: 1.5;
+  }
+</style>
